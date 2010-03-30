ad003115fc09a69ce0aa989b9a8c0f5a89bb3452
Sean Kibler <skibler@wrladv.com>
2010-03-30 16:03:34 -0400

    successfully but not happily parsing an xml response for SendSMSBasic from CDYNE, want to be using a library to build the test xml in rspec test, questioning XmlSimple library.

--- lib/sms_notify/response.rb
+++ lib/sms_notify/response.rb
@@ -1,10 +1,33 @@
+require 'xmlsimple'
 module SmsNotify
   class Response
     class << self
       # Accepts the response body from
       # CDYNE and turns it into a hash
       def parse(xml_response_body)
+        response_hash = xml_to_hash(xml_response_body)
+        response_hash.first[1].delete("xmlns")
+        response_hash
+      end
 
+      def xml_to_hash(xml)
+        parser = XmlSimple.new({'ForceArray' => false, 'KeepRoot' => true, 'SuppressEmpty' => nil})
+        normalize_keys(parser.xml_in(xml))
+      end
+
+      private
+      def normalize_keys(hash)
+        new_hash = {}
+        hash.first[1].each_pair { |k,v| new_hash.merge!(underscore(k) => v) }
+        {underscore(hash.first[0]) => new_hash}
+      end
+
+      def underscore(camel_cased_word)
+        camel_cased_word.to_s.gsub(/::/, '/').
+          gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+          gsub(/([a-z\d])([A-Z])/,'\1_\2').
+          tr("-", "_").
+          downcase
       end
     end
   end
--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -3,23 +3,37 @@ require 'builder'
 
 describe "Response" do
   before(:each) do
-    @response_body = ''
-
-    xml_builder = Builder::XmlMarkup.new(@response_body, :indent => 2)
-    xml_builder.instruct!
-    xml_builder.SmsReturn(:xmlns => 'http://ws.cdyne.com/SmsWS/') do |sms_return|
-      sms_return.StatusCode('0')
-      sms_return.StatusText('Success')
-      sms_return.Completed('1')
-      sms_return.TextID('12345678')
-      sms_return.Demo('0')
-      sms_return.ScheduledTime('')
-      sms_return.Responded('0')
-    end
+    @xml = <<EOXML
+<?xml version="1.0" encoding="utf-8"?>
+<SmsReturn xmlns="http://ws.cdyne.com/SmsWS/">
+  <StatusCode>1</StatusCode>
+  <StatusText>Success</StatusText>
+  <Completed>1</Completed>
+  <TextID>12345678</TextID>
+  <Demo>0</Demo>
+  <ScheduledTime></ScheduledTime>
+  <Responded>0</Responded>
+</SmsReturn>
+EOXML
+  end
+  
+  after(:each) do
+    @xml = ''
   end
 
   it "should convert an xml response body into a hash" do
-  
+    @response_hash = {
+      "sms_return" => {
+        "status_code"=>"1",
+        "status_text"=>"Success",
+        "completed"=>"1",
+        "text_id"=>"12345678",
+        "demo"=>"0",
+        "scheduled_time"=>nil,
+        "responded"=>"0"
+      }
+    }
+    SmsNotify::Response.parse(@xml).should == @response_hash
   end
 end
 
