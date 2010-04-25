0e67c38e75330b57559e7a4231daacb378a24135
Sean Kibler <github@skiblerspot.net>
2010-04-25 17:31:46 -0400

    rewrite Response.parse to handle a SOAP::Mapping::Object, uses SoapRespose class

--- lib/sms_notify/response.rb
+++ lib/sms_notify/response.rb
@@ -3,10 +3,17 @@ module SmsNotify
   # Handles parsing a CDYNE SmsNotify! XML response
   # into a Ruby-like hash.
   class Response
-    # Converts XML into a Ruby-like hash
-    def self.parse(xml_response_body)
-      parser = XmlSimple.new({'ForceArray' => false, 'KeepRoot' => true, 'SuppressEmpty' => nil})
-      parser.xml_in(xml_response_body)
-    end
+    class << self
+	    # Converts XML into a Ruby-like hash
+	    def parse(response)
+	      if response.is_a?(SOAP::Mapping::Object)
+          soap_response = SoapResponse.new(response)
+          soap_response.to_hash
+	      else
+	        parser = XmlSimple.new({'ForceArray' => false, 'KeepRoot' => true, 'SuppressEmpty' => nil})
+	        parser.xml_in(response)
+	      end
+	    end
+	  end
   end
 end
