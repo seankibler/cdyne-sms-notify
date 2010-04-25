8b31a74b1200564049caa90b97b8d09c61cd3f9d
Sean Kibler <github@skiblerspot.net>
2010-04-24 21:44:10 -0400

    MessageResponse is now responsible for exposing the response attributes in snake case

--- lib/sms_notify/message_response.rb
+++ lib/sms_notify/message_response.rb
@@ -14,11 +14,11 @@ module SmsNotify
     #     :ReceivedDate => '2010-04-03T14:29:14.7',
     #     :Message => 'foo')
     def initialize(attrs)
-      @response_id    = attrs[:ResponseID] || nil
-      @text_id        = attrs[:TextID] || nil
-      @status_code    = attrs[:StatusCode] || nil
-      @received_date  = attrs[:ReceivedDate] || nil
-      @message        = attrs[:Message] || nil
+      @response_id    = attrs["ResponseID"] || nil
+      @text_id        = attrs["TextID"] || nil
+      @status_code    = attrs["StatusCode"] || nil
+      @received_date  = attrs["ReceivedDate"] || nil
+      @message        = attrs["Message"] || nil
     end
   end
 end
