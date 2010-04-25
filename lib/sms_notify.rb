20fa94acaf348eb764edb148be01eb35b804e909
Sean Kibler <github@skiblerspot.net>
2010-04-25 17:28:32 -0400

    change bootstrapping to load up libraries in ext

--- lib/sms_notify.rb
+++ lib/sms_notify.rb
@@ -3,9 +3,11 @@ require 'rubygems'
 # Creates a wrapper namespace for the
 # rest of the API classes.
 module SmsNotify
-  Dir.glob(File.join(File.dirname(__FILE__), '/sms_notify/*')).each do |lib|
-    require lib
-  end
+  ['sms_notify', 'ext'].each { |dir|
+    Dir.glob(File.join(File.dirname(__FILE__), "/#{dir}/*")).each do |lib|
+      require lib
+    end
+  }
   
   class ConnectionError < StandardError;end
   class ApiError < StandardError;end
