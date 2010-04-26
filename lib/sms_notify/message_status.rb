fefd1a7c9468bc2bace93db87ffb01636683789b
Sean Kibler <github@skiblerspot.net>
2010-04-26 12:00:40 -0400

    the hash expected in MessageStatus initialization will either have keys of snake cased symbols or camel cased strings, and we must look for Demo key as well as :demo symbol

--- lib/sms_notify/message_status.rb
+++ lib/sms_notify/message_status.rb
@@ -9,22 +9,22 @@ module SmsNotify
     #
     # == Example
     #   SmsNotify::MessageStatus.new(
-    #     :Completed => '1',
-    #     :Demo => '0',
-    #     :ScheduledTime => '2010-04-03T14:29:14.7',
-    #     :StatusCode => '6',
-    #     :StatusText => 'Completed',
-    #     :ReceivedDate => '2010-04-03T14:29:14.7',
-    #     :Responded => '0')
+    #     "Completed"     => '1',
+    #     "Demo"          => '0',
+    #     "ScheduledTime" => '2010-04-03T14:29:14.7',
+    #     "StatusCode"    => '6',
+    #     "StatusText"    => 'Completed',
+    #     "ReceivedDate"  => '2010-04-03T14:29:14.7',
+    #     "Responded"     => '0')
     def initialize(attrs)
-      @completed      = attrs[:completed]       || attrs[:Completed]      || nil
-      @demo           = attrs[:demo]            || nil
-      @scheduled_time = attrs[:scheduled_time]  || attrs[:ScheduledTime]  || nil
-      @status_code    = attrs[:status_code]     || attrs[:StatusCode]     || nil
-      @status_text    = attrs[:status_text]     || attrs[:StatusText]     || nil
-      @text_id        = attrs[:text_id]         || attrs[:TextID]         || nil
-      @received_date  = attrs[:received_date]   || attrs[:ReceivedDate]   || nil
-      @responded      = attrs[:responded]       || attrs[:Responded]      || nil
+      @completed      = attrs[:completed]       || attrs["Completed"]      || nil
+      @demo           = attrs[:demo]            || attrs["Demo"]           || nil
+      @scheduled_time = attrs[:scheduled_time]  || attrs["ScheduledTime"]  || nil
+      @status_code    = attrs[:status_code]     || attrs["StatusCode"]     || nil
+      @status_text    = attrs[:status_text]     || attrs["StatusText"]     || nil
+      @text_id        = attrs[:text_id]         || attrs["TextID"]         || nil
+      @received_date  = attrs[:received_date]   || attrs["ReceivedDate"]   || nil
+      @responded      = attrs[:responded]       || attrs["Responded"]      || nil
     end
 
     def fatal_error?
